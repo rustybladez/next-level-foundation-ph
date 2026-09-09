147b44d1329f7cc71dc69ae5e87db6a3f64f737c
Zunaid Ali <zpokean@gmail.com>
2026-09-09 17:36:44 +0600

    Module 6 done

---
+++ conceptual class/index.js
@@ -0,0 +1,45 @@
+const arr = [5, 6, 7, 8, 9, 1, 2, 3, 4]
+
+// console.log(arr)
+
+// doesn't return anything
+
+// const foreacharr = arr.forEach(n => {
+//     console.log(typeof n)
+// })
+
+// returns an array []
+// const maparray = arr.map((n) => n + 2)
+// console.log(maparray)
+
+
+// const filterarr = arr.filter((n) => n % 2 === 0)
+
+// console.log(filterarr)
+
+// const findarr = arr.find((n) => n > 5)
+
+// console.log(findarr)
+// console.log(arr.sort())
+
+const user = {name: "john"}
+
+// Optional chaining. Prevent the app from crashing by returning 'undefined' for non-existent value
+// console.log(user.address?.city)
+
+// const pizza = {
+//     size: "large",
+//     topping: "cheese",
+//     price: 500
+// }
+
+// const { size, topping } = pizza
+
+// console.log(`I want a ${size} pizza with ${topping} topping`)
+
+// const fullName = ["Mr", "Spider", "Ahmad"]
+
+// const [first,,name] = fullName
+
+// console.log(first, name)
+
